a5a66cb992f29308c00a89aea7faa0050abb4f38
Pablo Buitrago <buitre@MacBook-Pro.local>
2019-05-08 01:19:00 -0300

    Amchart + Vuex added

---
+++ src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <Amchart />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Amchart from '@/components/Amchart.vue'
+
+export default {
+  name: 'about',
+  components: {
+    Amchart
+  }
+}
+</script>
